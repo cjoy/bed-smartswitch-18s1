aaa00aad371b121583708ae3d2a385a30d92d411
Alexander Jaroszewicz <alex.jaro@au1.ibm.com>
2018-05-12 17:11:17 +1000

    update the endpoints so the front end can connect

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const sql = require('mssql');
-const Client = require('azure-iothub').Client;
-const Message = require('azure-iot-common').Message;
-var bodyParser = require('body-parser');
+const express = require("express");
+const sql = require("mssql");
+const Client = require("azure-iothub").Client;
+const Message = require("azure-iot-common").Message;
+var bodyParser = require("body-parser");
 
 const dbConfig = {
-    user: 'comp6324admin',
-    password: 'COMP6324password',
-    server: 'smartswitch.database.windows.net',
-    database: 'smartswitch',
-    options: {
-        encrypt: true
-    }
+  user: "comp6324admin",
+  password: "COMP6324password",
+  server: "smartswitch.database.windows.net",
+  database: "smartswitch",
+  options: {
+    encrypt: true
+  }
 };
 
 const iotHubConfig = {
-    hostname: 'SmartSwitch.azure-devices.net',
-    sharedkey: '8Wrt9USCDDOQg/r4pK/IpZH6iRibnNHpv0wZ+GkL22M=',
-    sharedkeyname: 'iothubowner',
-}
+  hostname: "SmartSwitch.azure-devices.net",
+  sharedkey: "8Wrt9USCDDOQg/r4pK/IpZH6iRibnNHpv0wZ+GkL22M=",
+  sharedkeyname: "iothubowner"
+};
 
-const iotHubConnectionString = `HostName=${iotHubConfig.hostname};SharedAccessKeyName=${iotHubConfig.sharedkeyname};SharedAccessKey=${iotHubConfig.sharedkey}`;
+const iotHubConnectionString = `HostName=${
+  iotHubConfig.hostname
+};SharedAccessKeyName=${iotHubConfig.sharedkeyname};SharedAccessKey=${
+  iotHubConfig.sharedkey
+}`;
 const serviceClient = Client.fromConnectionString(iotHubConnectionString);
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
-
-  
 
 /**
  * HELPER FUNCTIONS
@@ -43,25 +48,21 @@ app.use(function(req, res, next) {
  * when it comes through showing the success state of sending
  * the message to the device.
  */
-function receiveFeedback(err, receiver)
-{
-    receiver.on('message', function (message) 
-    {
-        console.log('Feedback message:')
-        console.log(message.getData().toString('ascii'));
-    });
+function receiveFeedback(err, receiver) {
+  receiver.on("message", function(message) {
+    console.log("Feedback message:");
+    console.log(message.getData().toString("ascii"));
+  });
 }
 
 /*
  * Print the resulting status of the send action.
  */
-function printResultFor(op) 
-{
-    return function printResult(err, res) 
-    {
-        if (err) console.log(op + ' error: ' + err.toString());
-        if (res) console.log(op + ' status: ' + res.constructor.name);
-    };
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) console.log(op + " error: " + err.toString());
+    if (res) console.log(op + " status: " + res.constructor.name);
+  };
 }
 
 /**
@@ -71,56 +72,134 @@ function printResultFor(op)
 /** GET /
  * Get list of devices
  */
-app.get('/api/', (req, res) => {
-    sql.connect(dbConfig).then(pool => {
-        return pool.request()
-            .query('select * from devices');
-    }).then(result => {
-        console.log(result.recordset);
-        res.send(result.recordset);
-        sql.close();
-    }).catch(err => {
-        res.send('Unable to retreive devices');
-        sql.close();
+app.get("/api/", (req, res) => {
+  sql
+    .connect(dbConfig)
+    .then(pool => {
+      return pool.request().query("select * from devices");
+    })
+    .then(result => {
+      console.log(result.recordset);
+      res.send(result.recordset);
+      sql.close();
+    })
+    .catch(err => {
+      res.send("Unable to retreive devices");
+      sql.close();
+    });
+});
+
+app.get("/api/v2/devices", (req, res) => {
+  sql
+    .connect(dbConfig)
+    .then(pool => {
+      return pool
+        .request()
+        // .query(
+        //   `select deviceId, status from devices order by  group by d.deviceId`
+        // );
+        .query(`select DISTINCT a.deviceId, b.status, b.last_update from user_devices a JOIN sensor_data b on a.deviceId= b.deviceId where b.last_update = (select max(last_update) from sensor_data) and a.username = 'test' and b.deviceid = 'Device1';`)
+    })
+    .then(result => {
+      res.send(result.recordset);
+      sql.close();
+    })
+    .catch(err => {
+      res.send(err);
+      sql.close();
+    });
+});
+
+// all devices usage for current month
+app.get("/api/v2/devices/month", (req, res) => {
+  var month = new Date().getMonth() + 1;
+  sql
+    .connect(dbConfig)
+    .then(pool => {
+      return pool
+        .request()
+        .input("month", sql.VarChar, month)
+        .query(
+          `select deviceId, sum(cast(usage as float)) usage from sensor_data where month = @month group by deviceId order by deviceId`
+        );
+    })
+    .then(result => {
+      res.send(result.recordset);
+      sql.close();
+    })
+    .catch(err => {
+      res.send(err);
+      sql.close();
+    });
+});
+
+// all devices usage for a given month
+app.get("/api/v2/devices/:month", (req, res) => {
+  sql
+    .connect(dbConfig)
+    .then(pool => {
+      return pool
+        .request()
+        .input("month", sql.VarChar, req.params.month)
+        .query(
+          `select deviceId, sum(cast(usage as float)) usage from sensor_data where month = @month group by deviceId`
+        );
+    })
+    .then(result => {
+      res.send(result.recordset);
+      sql.close();
+    })
+    .catch(err => {
+      res.send(err);
+      sql.close();
     });
 });
 
 /** GET /:deviceid  (eg. GET /Device1)
- * Get a single device's states 
+ * Get a single device's states
  */
-app.get('/api/:deviceid', (req, res) => {
-    sql.connect(dbConfig).then(pool => {
-        return pool.request()
-            .input('deviceId', sql.VarChar, req.params.deviceid)
-            .query(`select * from sensor_data where deviceId = @deviceId`);
-    }).then(result => {
-        res.send(result.recordset);
-        sql.close();
-    }).catch(err => {
-        res.send(err);
-        sql.close();
+app.get("/api/v1/:deviceid", (req, res) => {
+  sql
+    .connect(dbConfig)
+    .then(pool => {
+      return pool
+        .request()
+        .input("deviceId", sql.VarChar, req.params.deviceid)
+        .query(`select * from sensor_data where deviceId = @deviceId`);
+    })
+    .then(result => {
+      res.send(result.recordset);
+      sql.close();
+    })
+    .catch(err => {
+      res.send(err);
+      sql.close();
     });
 });
 
 /** POST /:deviceid
  * Send a device message to the iot hub
+ * {
+ *  status: checked/unchecked
+ * }
  */
-app.post('/api/:deviceid', (req, res) => {
-    serviceClient.open((err) => {
-        if (err) {
-            res.send(`Could not connect: ${err.message}`);
-        } 
-        else {
-            console.log('Service client connected');
-            serviceClient.getFeedbackReceiver(receiveFeedback);
-            let message = new Message(JSON.stringify(req.body));
-            message.ack = 'full';
-            console.log('Sending message: ', req.body);
-            serviceClient.send(req.params.deviceid, message, printResultFor('send'));
-            res.send({ message: req.body, status: 'OK'});
-        }
-    });
+app.post("/api/v1/:deviceid", (req, res) => {
+  serviceClient.open(err => {
+    if (err) {
+      res.send(`Could not connect: ${err.message}`);
+    } else {
+      console.log("Service client connected");
+      serviceClient.getFeedbackReceiver(receiveFeedback);
+      let message = new Message(JSON.stringify(req.body));
+      message.ack = "full";
+      console.log("Sending message: ", req.body);
+      serviceClient.send(req.params.deviceid, message, printResultFor("send"));
+      res.send({ message: req.body, status: "OK" });
+    }
+  });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Smartswitch REST API listening on port ${port}!`));
+const port = process.env.PORT || 3000; // update this so that the front end can connect locally.
+app.listen(port, () =>
+  console.log(`Smartswitch REST API listening on port ${port}!`)
+);
