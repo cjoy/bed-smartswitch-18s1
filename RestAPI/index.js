56627cda387b577ef108b9c691bda6696dfb3a81
Shawn <shawn_rox_this_world@hotmail.com>
2018-05-19 09:53:54 +0000

    Fixing rooms endpoint

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -9,7 +9,7 @@ const azure = require("./AzureConfig");
 
 const dbConfig = azure.dbConfig;
 const sqlConnPool = new sql.ConnectionPool(dbConfig);
-sqlConnPool.connect().then(() => {console.log("Database connected");})
+sqlConnPool.connect().then(() => {console.log("Database connected");});
 
 const iotHubConnectionString = azure.IoTHubConnectionString;
 const serviceClient = Client.fromConnectionString(iotHubConnectionString);
@@ -49,6 +49,11 @@ cloudMon.monitorCloud(iotHubConnectionString, function(message)
             .input("deviceId", sql.VarChar, deviceId)
             .input("newStatus", sql.VarChar, newStatus)
             .query("UPDATE registered_devices SET status=@newStatus WHERE deviceId=@deviceId");
+        var request2 = new sql.Request(sqlConnPool);
+        request2
+            .input("deviceId", sql.VarChar, deviceId)
+            .input("newStatus", sql.VarChar, newStatus)
+            .query("UPDATE registered_devices_copy SET status=@newStatus WHERE deviceId=@deviceId");
     }
 });
 
@@ -100,18 +105,19 @@ app.post("/api/v1/user/sign-in", (req, res) =>
     request
         .input("username", sql.VarChar, req.body.username)
         .input("password", sql.VarChar, req.body.password)
-        .query(`SELECT id FROM users WHERE password=@password and username=@username`, function(err, result) 
+        .query(`SELECT customerId, username FROM users_copy WHERE password=@password and username=@username`, function(err, result) 
         {
             if (err) 
             {
                 console.error(err);
-                res.status(500).send({ status: false, userId: -1, error: err });
+                res.status(500).send({ status: false, userId: -1, username: "None", error: err });
                 return;
             }
-            console.log(result)
+            console.log(result.recordset)
             const status = result.recordset.length !== 0 ? true : false;
-            const userId = status ? result.recordset[0].id : -1;
-            res.status(200).send({ status, userId, error: null });
+            const userId = status ? result.recordset[0].customerId : -1;
+            const username = status ? result.recordset[0].username : "None";
+            res.status(200).send({ status, userId, username, error: null });
         });
 });
 
@@ -183,6 +189,84 @@ app.get("/api/v2/devices/data/month/:month", (req, res) =>
 
 //#endregion
 
+//#region  ======================================  DEVICES : GET BUILDING DEVICE INFO   ================================ //
+
+/** 
+ * GET /
+ * Searches for the tuple (deviceid,productid,roomid,devicetype,devicename,status) from the database and 
+ * categorises the devices according to product.
+ */
+app.get("/api/v3/buildings/devices/:customerId", (req, res) => 
+{
+    var request = new sql.Request(sqlConnPool);
+    request
+        .input("customerId", sql.VarChar, req.body.customerId)
+        .query(`
+            SELECT * FROM registered_devices_copy 
+            WHERE productId IN (SELECT productId FROM products WHERE customerId=@customerId)
+        `,
+        function(err, result)
+        {
+            if (err) 
+            {
+                console.error(err);
+                res.status(500).send(err.message);
+                return;
+            }
+            const records = {};
+            result.recordset.forEach((record) =>
+            {
+                if (!records[record.productId]) 
+                {
+                    records[record.productId] = []
+                }
+                records[record.productId].push(record)
+            });
+            const products = {};
+            var i = 1;
+            const productData = [];
+            Object.keys(records).forEach((product) =>
+            {
+                const rooms = {};
+                records[product].forEach((device) => 
+                {
+                    if (!rooms[device.roomid]) 
+                    {
+                        rooms[device.roomid] = []
+                    }
+                    rooms[device.roomid].push(
+                    {
+                        id: device.deviceId,
+                        type: device.device_type,
+                        name: device.device_name,
+                        status: device.status === 'Off' ? false : true,
+                    });
+                });
+                var j = 1;
+                const roomData = [];
+                Object.keys(rooms).forEach(room => 
+                {
+                    data = 
+                    {
+                        roomID: j,
+                        name: room,
+                        devices: rooms[room],
+                    };
+                    j += 1;
+                    roomData.push(data);
+                })
+                productData.push(
+                {
+                    productID: i,
+                    rooms: roomData
+                })
+                i += 1;
+            });
+            console.log(JSON.stringify(productData));
+            res.status(200).send(productData);
+        });
+});
+
 //#region  ======================================  DEVICES : GET ROOM DEVICE INFO   ================================ //
 
 /** 
@@ -192,10 +276,15 @@ app.get("/api/v2/devices/data/month/:month", (req, res) =>
  */
 app.get("/api/v3/room/devices/:username", (req, res) => 
 {
+    console.log(req.params);
     var request = new sql.Request(sqlConnPool);
     request
-        .input("username", sql.VarChar, req.params.username)
-        .query("SELECT * FROM registered_devices WHERE username = @username", function(err, result) 
+        .input("customerId", sql.VarChar, req.params.username)
+        .query(`
+            SELECT * FROM registered_devices_copy 
+            WHERE productId IN (SELECT productId FROM products WHERE customerId=@customerId)
+        `,
+        function(err, result)
         {
             if (err) 
             {
@@ -228,6 +317,7 @@ app.get("/api/v3/room/devices/:username", (req, res) =>
                     name: room,
                     devices: rooms[room],
                 };
+                i += 1
                 roomData.push(data);
             })
             res.status(200).send(roomData);
@@ -323,7 +413,7 @@ app.post("/api/v1/unregister/device/", (req, res) =>
                     res.status(500).send(err);
                     return;
                 }
-                console.log("Unregister: " + result)
+                console.log(`Unregister: ${req.body.username} ${req.body.deviceId}` + result)
                 res.status(200).send({ message: "Success" });
             });
 });
